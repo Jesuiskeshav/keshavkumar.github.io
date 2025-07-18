fb86f9755a17a68a48e8e720bf2f9b3eb32834b3
Keshav Kumar <jesuiskeshav@gmail.com>
2025-07-18 06:28:16 +0530

    javascript.js

---
+++ javascript.js
@@ -0,0 +1,15 @@
+ tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        primary: '#6366f1',
+                        secondary: '#f9fafb',
+                        accent: '#f3f4f6',
+                        dark: '#374151',
+                    },
+                    fontFamily: {
+                        sans: ['Inter', 'sans-serif'],
+                    },
+                }
+            }
+ }
